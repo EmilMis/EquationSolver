5823a0fbd34140257885602af9f50175a7929a41
emilm <emil.missoni@gmail.com>
2021-08-10 14:37:08 +0200

    Now added some messages and fixed few bugs

--- src/empi/EquationSolver.java
+++ src/empi/EquationSolver.java
@@ -1,6 +1,7 @@
 package empi;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class EquationSolver {
     public void SolveGivenEquation(String equation){
@@ -10,7 +11,7 @@ public class EquationSolver {
         ArrayList<String> equationNeat = utils.neatify(equationArrayList);
         double solution = utils.solve(equationNeat);
         long endTime = System.nanoTime();
-        System.out.println("Solved in " + ((endTime - startTime) / 1000000) + " milliseconds");
+        System.out.println("Solved in " + ((endTime - startTime) / 1000000) + " milliseconds,\n" + getMessage((endTime - startTime) / 1000000) + "\n");
         long[] numbers = utils.getNumbers(equationNeat);
         long[] xs = utils.getXs(equationNeat);
         System.out.println(equation);
@@ -18,13 +19,34 @@ public class EquationSolver {
         System.out.println(equationNeat);
         System.out.println(xs[0] + "x + " + numbers[0] +  " = "+ xs[1] + "x + " + numbers[1]);
         System.out.println((xs[0] - xs[1]) + "x = " + (numbers[1] - numbers[0]));
-        System.out.println("x = " + solution);
+        System.out.print("x = " + (numbers[1] - numbers[0]) + "/" + (xs[0] - xs[1]) +  " = " + solution + " ≈ ");
+        System.out.printf("%.0f", solution);
     }
     public void SolveEquation(){
         Utils utils = new Utils();
         System.out.print("Write your equation here >>> ");
         String equation = utils.getEquationRaw();
         SolveGivenEquation(equation);
+    }
+
+    private String getMessage(long delay){
+        String[] messages = {
+                "enough time for me to eat an ice cream",
+                "ehough time for a c++ developer to open StackOverflow",
+                "time needed to solve this equation (wow)",
+                "enough time for light to\ntravel " + Float.toString((float) (299792458 / 1000) * delay) + " meters",
+                "enough time for a snail to\ncross " + Float.toString((float) (48000000 / (60 * 60 * 1000)) * delay) + " nanometers",
+                "time needed for a cheetah to\ncross " + Float.toString((float) (120000000 / (60 * 60 * 1000)) * delay) + " millimeters\nWhen at full speed, of course",
+                "I ran out of ideas to put here",
+                "time necessary to find stupid things online",
+                "t1me n3eded f0r mY c4T t0 f3ll asl3ep My<<<<< K3yb0ard---------!",
+                "way less time to put another good fact here",
+                "c++ is faster, though",
+                "time needed for Windows to crash",
+                "My ccat 1s st1ll H3re-..,,>"
+        };
+        Random random = new Random();
+        return messages[random.nextInt(messages.length)];
     }
 
 }
--- src/empi/Main.java
+++ src/empi/Main.java
@@ -2,10 +2,9 @@ package empi;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("welcome to Equation Solver!\nWritten and debugged by EmilMis\nHave fun!\nOh, and a warning:\nIf you go nuts with numbers (which undoubtedly you will)\nprogram is going to crash because they're to big\nfor a variable to store\n\n\n");
         EquationSolver equationSolver = new EquationSolver();
         equationSolver.SolveEquation();
-        long number1 = 234234234234244L;
-        long number2 = 45345343535343L;
-        System.out.println((float) number1 / number2);
+
     }
 }
